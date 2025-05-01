3213a36f805420922ce8ce854886a498c0d06073
Reshma <reshmajawale07@gmail.com>
2025-05-01 21:27:54 +0700

    port change

--- nested_accodion_2.py
+++ nested_accodion_2.py
@@ -215,5 +215,5 @@ def generate_referral_and_redirect(n_clicks, values, ids):
     return "/referral"
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
-
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
