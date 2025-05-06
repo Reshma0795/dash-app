77fed7b039e5d02f994cb46df389bd4ec4dc1159
Reshma <reshmajawale07@gmail.com>
2025-05-06 17:29:31 +0800

    port changed

--- nested_accodion_2.py
+++ nested_accodion_2.py
@@ -305,6 +305,6 @@ def sync_selected_options(values_all, ids_all):
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 8050))
-    #app.run_server(debug=True, host="0.0.0.0", port=port)
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
+    #app.run_server(debug=True)
